4090c58826e79e79498f20a8f43ac00409c4705b
Quang Trong <80852103+quangtrong1506@users.noreply.github.com>
2023-07-10 02:22:00 +0700

    -

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ db.connect();
 //set view engine
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', 'demo_web_ban_hang/views');
+app.set('views', '/var/task/views');
 // set public directory path.join('/var/task/views')
 app.use(express.static(path.join('/public')));
 
